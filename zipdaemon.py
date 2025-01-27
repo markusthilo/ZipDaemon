8887e186f9abb6c27ab9a99b1e5ee24b145c6ee8
markusthilo <markus.thilo@gmail.com>
2025-01-27 08:50:00 +0100

    Changed path of zips

--- zipdaemon.py
+++ zipdaemon.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 __author__ = 'Markus Thilo'
-__version__ = '0.0.1_2025-01-22'
+__version__ = '0.0.1_2025-01-27'
 __license__ = 'GPL-3'
 __email__ = 'markus.thilo@gmail.com'
 __status__ = 'Testing'
